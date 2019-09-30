47aa3df25e32a475ea3f9a60352344027d8708b1
karthik pamidimukkala <karthiksai231@gmail.com>
2019-09-30 00:05:40 -0400

    Initial Commit

---
+++ movie-catalog-service/src/main/java/com/spring/project/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,38 @@
+package com.spring.project.moviecatalogservice.resources;
+
+import com.spring.project.moviecatalogservice.models.CatalogItem;
+import com.spring.project.moviecatalogservice.models.Movie;
+import com.spring.project.moviecatalogservice.models.Rating;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId)
+    {
+        RestTemplate restTemplate = new RestTemplate();
+
+        List<Rating> ratings = Arrays.asList(
+           new Rating("foo", 4)
+        );
+
+        return ratings.stream().map(rating -> {
+            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+            return new CatalogItem(movie.getName(), "Desc", rating.getRating());
+        }).collect(Collectors.toList());
+
+        /* return Collections.singletonList(
+                new CatalogItem("Transformers", "Test", 4)
+        );*/
+    }
+}
---
+++ movie-info-service/src/main/java/com/spring/project/movieinfoservice/resources/MovieResource.java
@@ -0,0 +1,16 @@
+package com.spring.project.movieinfoservice.resources;
+
+import com.spring.project.movieinfoservice.models.Movie;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+
+    @RequestMapping("/{movieId}")
+    public Movie getMovie(@PathVariable("movieId") String movieId) {
+        return new Movie(movieId, "Test name");
+    }
+}
